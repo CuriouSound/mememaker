0edad9eb8087a063227cac2ddcb709e9b1206a75
CuriouSound <robin.nota68@gmail.com>
2018-12-04 16:27:55 +0100

    add sql

---
+++ table.sql
@@ -0,0 +1,27 @@
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
+
+
+#------------------------------------------------------------
+# Table: images_brutes
+#------------------------------------------------------------
+
+CREATE TABLE images_brutes(
+        ID_imgbrutes  Int  Auto_increment  NOT NULL ,
+        Url_imgbrutes Varchar (1000) NOT NULL
+	,CONSTRAINT images_brutes_PK PRIMARY KEY (ID_imgbrutes)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: memes
+#------------------------------------------------------------
+
+CREATE TABLE memes(
+        Id_memes  Int  Auto_increment  NOT NULL ,
+        img_memes Varchar (1000) NOT NULL ,
+        titre     Varchar (255) NOT NULL
+	,CONSTRAINT memes_PK PRIMARY KEY (Id_memes)
+)ENGINE=InnoDB;
+
