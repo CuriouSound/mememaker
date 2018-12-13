aca0fc3aff004df3a6a34ce8c86e4f793e7c4580
Morgannec <morganne.culas@hotmail.fr>
2018-12-13 09:14:55 +0100

    modif

--- Models/connectionBDD.php
+++ Models/connectionBDD.php
@@ -1,23 +1,25 @@
 <?php
-
 $host = '127.0.0.1';
-	$db = '';
-	$user ='root';
-	$pass = 'Lula2018';
-	$charset = 'utf8mb4';
+$db = '';
+$user = 'root';
+$pass = 'Lula2018';
+$charset = 'utf8mb4';
 
-	$dbh = "mysql:host=$host;dbname=$db;charset=$charset";
-	$options = [
-		PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-		PDO::ATTR_EMULATE_PREPARES   => false,
-	];
+$dbh = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	PDO::ATTR_EMULATE_PREPARES => false,
+];
 
-	try {
-		$dbh = new PDO($dbh, $user, $pass, $options);
-	
+try {
+	$dbh = new PDO($dbh, $user, $pass, $options);
 
-	} catch (PDOException $e) {
-		echo 'Connexion échouée : ' . $e->getMessage();
-	}
 
+} catch (PDOException $e) {
+	echo 'Connexion échouée : ' . $e->getMessage();
+}
+
+$memesgenerator = $bdd->prepare('INSERT INTO `Id_memes`( `url_memes`, `titre`, `cheminlocal` VALUES ("value-1","value-2","value-3","value-4")');
+return $memesgenerator->execute();
+}
