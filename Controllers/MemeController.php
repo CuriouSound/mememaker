d1610148be4f552ff2623be6c792d73de7bb07b5
CuriouSound <robin.nota68@gmail.com>
2018-12-13 09:50:48 +0100

    fix image carousel, bug image

--- Controllers/MemeController.php
+++ Controllers/MemeController.php
@@ -3,11 +3,17 @@
 include('Models/Images_Brutes.php');
 include('Models/Meme.php');
 
-$randomNum = rand(1, 45);
-$randomNum1 = rand(1, 45);
-$randomNum2 = rand(1, 45);
-$randomNum3 = rand(1, 45);
-$randomNum4 = rand(1, 45);
+$randomNum = rand(1, 44);
+$randomNum1 = rand(1, 44);
+$randomNum2 = rand(1, 44);
+$randomNum3 = rand(1, 44);
+$randomNum4 = rand(1, 44);
+$randomNum5 = rand(1, 44);
+$randomNum6 = rand(1, 44);
+$randomNum7 = rand(1, 44);
+$randomNum8 = rand(1, 44);
+$randomNum9 = rand(1, 44);
+
 
  
  
--- Models/connectionBDD.php
+++ Models/connectionBDD.php
@@ -2,7 +2,7 @@
 $host = '127.0.0.1';
 $db = '';
 $user = 'root';
-$pass = 'Lula2018';
+$pass = '';
 $charset = 'utf8mb4';
 
 $dbh = "mysql:host=$host;dbname=$db;charset=$charset";
@@ -18,8 +18,4 @@ try {
 
 } catch (PDOException $e) {
 	echo 'Connexion échouée : ' . $e->getMessage();
-}
-
-$memesgenerator = $bdd->prepare('INSERT INTO `Id_memes`( `url_memes`, `titre`, `cheminlocal` VALUES ("value-1","value-2","value-3","value-4")');
-return $memesgenerator->execute();
 }
