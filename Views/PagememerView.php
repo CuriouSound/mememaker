7fb6875cca810e512fb30f2aee5d1657fd897969
Morgannec <morganne.culas@hotmail.fr>
2018-12-14 11:45:13 +0100

    modification du css

--- Views/PagememerView.php
+++ Views/PagememerView.php
@@ -2,15 +2,18 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="morjannestyle.css"> 
+    <link rel="stylesheet" type="text/css" media="screen and (max-width: 1280px)" href="morjannestyle.css"> 
 </head>
 <body>
-<?php include('Views/HeaderView.php'); ?>
+<?php include('Views/HeaderView.php'); 
+
+?>
     <img src="memere.jpg" class="imaaage">
-    <p style="margin:auto; position: absolue;">Un meme'R c'est  les « mèmes » peuvent prendre la forme d'images, de vidéos ou encore de sons. <br>
+    <p class="textememer">Un meme'R c'est  les « mèmes » peuvent prendre la forme d'images, de vidéos ou encore de sons. <br>
         À visée humoristique, le « mème » existe dès qu'une personne lui donne un caractère comique en y apposant une légende, un commentaire ou en le détournant. <br>
        Et plus un « mème » est personnalisé par un grand nombre d'internaute, plus il devient viral.<br>
        Un exemple ? Chuck Norris. <br>
