089dea759340a12f2436ba2652a2d9bfb7de86e7
CuriouSound <robin.nota68@gmail.com>
2018-12-17 09:30:59 +0100

    need pull

--- Views/HomeView.php
+++ Views/HomeView.php
@@ -3,4 +3,21 @@
 <h1>Générateur de memes</h1>
 
 <a href="?page=meme"> clique ici pour faire ta mémère</a>
+
+<div id="slider">
+<figure>
+<img class=="imgsliderhome" src="memere.jpg" alt>
+<img class=="imgsliderhome" src="memere.jpg" alt>
+<img class=="imgsliderhome" src="memere.jpg" alt>
+<img class=="imgsliderhome" src="memere.jpg" alt>
+<img class=="imgsliderhome" src="memere.jpg" alt>
+</figure>
+<figure>
+    <img class=="imgsliderhome2" src="memere.jpg" alt>
+    <img class=="imgsliderhome2" src="memere.jpg" alt>
+    <img class=="imgsliderhome2" src="memere.jpg" alt>
+    <img class=="imgsliderhome2" src="memere.jpg" alt>
+    <img class=="imgsliderhome2" src="memere.jpg" alt>
+</figure>   
+</div>
 <?php include('Views/FooterView.php'); ?>
